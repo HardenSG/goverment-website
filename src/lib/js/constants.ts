bb81c66d0884f92846e994103f34d46d717d8b2b
yandeqiang_i <yandeqiang_i@didiglobal.com>
2023-08-28 23:50:42 +0800

    接口地址枚举 & 固定接口地址

--- src/lib/js/constants.ts
+++ src/lib/js/constants.ts
@@ -6,3 +6,18 @@ export const STATIC_IMAGES = {
     headerImage: ''
   }
 }
+
+/**
+ * 后台地址枚举
+ */
+export const BACKEND_SERVICE_IP = {
+  ONLINE: 'https://localhost:8080',
+  OFFLINE: 'http://localhost:8080'
+}
+
+/**
+ * 服务接口地址
+ */
+export const SERVICE_IP = process.env.ONLINE
+  ? BACKEND_SERVICE_IP['ONLINE']
+  : BACKEND_SERVICE_IP['OFFLINE']
